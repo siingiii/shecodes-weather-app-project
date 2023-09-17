6a52aef184eb479e67e92b3b5fa4d3e3a82e16a7
Singita Innocentia B <singitabaloyi@yahoo.com>
2023-09-18 00:28:18 +0200

    updated js so it shows current location and temperature

--- src/index.js
+++ src/index.js
@@ -1,86 +1,119 @@
-function showPosition(position) {
-  let lat = position.coords.latitude;
-  let lon = position.coords.longitude;
+let apiKey = "eb9542c65e739e0fb25ade97c749e2aa";
+const cityInput = document.getElementById("cityInput");
+const searchButton = document.getElementById("searchButton");
+const currentButton = document.getElementById("currentButton");
+const cityDisplay = document.getElementById("cityDisplay");
+const descriptionElement = document.getElementById("description");
+const temperatureElement = document.getElementById("temperature");
+const humidityElement = document.getElementById("humidity");
+const speedElement = document.getElementById("speed");
+const datetimeTextElement = document.getElementById("datetimeText");
 
-  console.log(position.coords.latitude);
-  console.log(position.coords.longitude);
-  let apiKey = "eb9542c65e739e0fb25ade97c749e2aa";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
-  const axios = require("axios/dist/browser/axios.cjs");
-  axios
-    .get(apiUrl)
-    .then(showTemperature)
-    .catch((error) => {
-      console.error("Axios Error:", error);
-    });
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
+  let day = days[date.getDay()];
+  return `${day}, ${hours}:${minutes}`;
 }
 
-function showTemperature(response) {
-  let data = response.data;
-  let cityDisplay = document.getElementById("cityDisplay");
-  cityDisplay.innerHTML = `${data.name}<br>${data.dt}<br>${data.weather[0].description}`;
-  let temperature = Math.round(data.main.temp);
-  let currentTemperature = document.querySelector(".sun-cloud");
-  currentTemperature.innerHTML = `${temperature} °C`;
-  let details = document.querySelector("h4");
-  details.innerHTML = `Precipitation: ${data.clouds.all}%<br>Humidity: ${data.main.humidity}%<br>Wind: ${data.wind.speed} km/h`;
-}
-function fetchWeatherByCity(city) {
-  let apiKey = "eb9542c65e739e0fb25ade97c749e2aa"; // Replace with your OpenWeatherMap API key
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-  let axios = require("axios/dist/browser/axios.cjs");
-  axios
-    .get(apiUrl)
-    .then(showTemperature)
-    .catch((error) => {
-      console.error("Axios Error:", error);
-    });
-}
-navigator.geolocation.getCurrentPosition(showPosition);
+function displayWeatherData(data) {
+  cityDisplay.textContent = data.name;
+  descriptionElement.textContent = data.weather[0].description;
+  temperatureElement.textContent = Math.round(data.main.temp);
+  humidityElement.textContent = data.main.humidity;
+  speedElement.textContent = Math.round(data.wind.speed);
+  datetimeTextElement.textContent = formatDate(data.dt * 1000);
 
-document.getElementById("searchButton").addEventListener("click", function () {
-  let cityInput = document.getElementById("cityInput");
-  let city = cityInput.value.trim();
+  const dayOfWeek = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+  ][new Date(data.dt * 1000).getDay()];
+}
+searchButton.addEventListener("click", () => {
+  const city = cityInput.value.trim();
   if (city !== "") {
-    fetchWeatherByCity(city);
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios
+      .get(apiUrl)
+      .then((response) => {
+        const dayOfWeek = [
+          "Sunday",
+          "Monday",
+          "Tuesday",
+          "Wednesday",
+          "Thursday",
+          "Friday",
+        ][new Date(response.data.dt * 1000).getDay()];
+
+        const currentDate = new Date();
+        const hours = currentDate.getHours();
+        const minutes = currentDate.getMinutes();
+        const formattedTime = `${hours < 10 ? "0" : ""}${hours}:${
+          minutes < 10 ? "0" : ""
+        }${minutes}`;
+
+        displayWeatherData(response.data, false);
+        datetimeTextElement.textContent = `${dayOfWeek}, ${formattedTime}`;
+      })
+      .catch((error) => {
+        console.error("Error fetching weather data:", error);
+      });
   }
 });
 
-document.getElementById("currentButton").addEventListener("click", function () {
-  showPosition();
-});
+function displayWeatherData(data, isCurrent) {
+  cityDisplay.textContent = data.name;
+  descriptionElement.textContent = data.weather[0].description;
+  temperatureElement.textContent = Math.round(data.main.temp);
+  humidityElement.textContent = data.main.humidity;
+  speedElement.textContent = Math.round(data.wind.speed);
 
-let searchButton = document.getElementById("searchButton");
-let currentButton = document.getElementById("currentButton");
-
-searchButton.addEventListener("click", function () {
-  let cityInput = document.getElementById("cityInput");
-  let cityName = cityInput.value.trim();
-  if (cityName !== "") {
-    fetchWeatherByCity(cityName);
+  if (!isCurrent) {
+    datetimeTextElement.textContent = formatDate(data.dt * 1000);
   }
+}
+
+currentButton.addEventListener("click", () => {
+  navigator.geolocation.getCurrentPosition((position) => {
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+    axios
+      .get(apiUrl)
+      .then((response) => {
+        const dayOfWeek = [
+          "Sunday",
+          "Monday",
+          "Tuesday",
+          "Wednesday",
+          "Thursday",
+          "Friday",
+        ][new Date(response.data.dt * 1000).getDay()];
+
+        const currentDate = new Date();
+        const hours = currentDate.getHours();
+        const minutes = currentDate.getMinutes();
+        const formattedTime = `${hours < 10 ? "0" : ""}${hours}:${
+          minutes < 10 ? "0" : ""
+        }${minutes}`;
+
+        displayWeatherData(response.data, true);
+        datetimeTextElement.textContent = `${dayOfWeek}, ${formattedTime}`;
+      })
+      .catch((error) => {
+        console.error("Error fetching weather data:", error);
+      });
+  });
 });
-
-currentButton.addEventListener("click", function () {
-  navigator.geolocation.getCurrentPosition(showPosition);
-});
-
-//
-
-let currentDate = new Date();
-let dayOfWeek = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-][currentDate.getDay()];
-let hours = currentDate.getHours();
-let minutes = currentDate.getMinutes();
-let formattedTime = `${hours < 10 ? "0" : ""}${hours}:${
-  minutes < 10 ? "0" : ""
-}${minutes}`;
-let datetimeBr = document.getElementById("datetime");
-datetimeBr.textContent = `${dayOfWeek}, ${formattedTime}`;
