09a0208306118bf466ed324a89ac9355d4fefe30
Singita Innocentia B <singitabaloyi@yahoo.com>
2023-10-09 00:14:48 +0200

    updated forecast temperatures with real time data

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 let currentUnit = "Celsius";
-
+//let timezoneOffset = 0;//
+//let timezoneOffset = response.data.timezone;//
 // Function to format the date and time
 function formatDate(timestamp, timezoneOffset) {
+  let localTimestamp = timestamp + timezoneOffset;
   let date = new Date(timestamp * 1000 + timezoneOffset * 1000);
+  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
+  let day = days[date.getUTCDay()];
   let hours = date.getUTCHours();
   if (hours < 10) {
     hours = `0${hours}`;
@@ -11,22 +15,10 @@ function formatDate(timestamp, timezoneOffset) {
   if (minutes < 10) {
     minutes = `0${minutes}`;
   }
-  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
-  let day = days[date.getUTCDay()];
+
   return `${day}, ${hours}:${minutes}`;
 }
 // Function to fetch weather data by city name
-function searchCity(city) {
-  let apiKey = "eb9542c65e739e0fb25ade97c749e2aa";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-
-  axios
-    .get(apiUrl)
-    .then(showTemperature)
-    .catch((error) => {
-      console.error("Error fetching weather data:", error);
-    });
-}
 
 // Function to display weather data
 function showTemperature(response) {
@@ -34,12 +26,41 @@ function showTemperature(response) {
   let f = document.querySelector("#deg-far");
   let c = document.querySelector("#deg-cel");
   let cel = Math.round(response.data.main.temp);
-  let timezoneOffset = response.data.timezone;
+  timezoneOffset = response.data.timezone;
   let description = response.data.weather[0].description;
   let capitalizedDescription =
     description.charAt(0).toUpperCase() + description.slice(1);
   let h2Element = document.querySelector("h2");
   let temperatureElement = document.querySelector("#temperature");
+
+  const days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  if (typeof timezoneOffset === "undefined") {
+    timezoneOffset = 0;
+  }
+  const currentUTCDay = new Date().getUTCDay();
+  const day = days[currentUTCDay];
+  const datetimeTextElement = document.querySelector("#datetimeText");
+  datetimeTextElement.innerHTML = day;
+  const timestamp = response.data.dt; // API provides the timestamp in local time
+
+  const localTimestamp = timestamp + timezoneOffset;
+  const date = new Date(localTimestamp * 1000);
+  //const day = days[date.getUTCDay()];//
+
+  // Update the day element in your HTML
+  //const datetimeTextElement = document.querySelector("#datetimeText");
+  datetimeTextElement.innerHTML = day;
+
+  console.log("Timestamp:", timestamp);
+  console.log("Timezone Offset:", timezoneOffset);
 
   if (h2Element) {
     h2Element.innerHTML = response.data.name;
@@ -81,18 +102,65 @@ function showTemperature(response) {
 
       document.querySelector("#temperature").innerHTML = `${cel}°C`;
       currentUnit = "Celsius";
-    });
 
-    // Update the date and time
-    function updateDateTime() {
-      const dateTimeText = document.querySelector("#datetimeText");
-      const currentTimestamp = Date.now() / 1000;
-      dateTimeText.innerHTML = formatDate(currentTimestamp, timezoneOffset);
-    }
-    updateDateTime();
-    setInterval(updateDateTime, 60000);
+      updateDateTime(timezoneOffset);
+      setInterval(() => updateDateTime(timezoneOffset), 60000);
+    });
   }
 }
+
+function forecastTemp(response) {
+  console.log(response.data.daily);
+  let forecast = response.data.daily;
+  let forecastElt = document.querySelector("#forcast");
+  let forecastHTML = `<div class="row">`;
+
+  forecast.forEach(function (forecastDay, index) {
+    console.log(forecastDay);
+    if (index > 0 && index < 6) {
+      if (forecastDay.dt && response.data.timezone_offset) {
+        let day = formatDate(forecastDay.dt);
+        let iconCode = forecastDay.weather[0].icon;
+        let description = forecastDay.weather[0].description;
+        let maxTemp = Math.round(forecastDay.temp.max);
+        let minTemp = Math.round(forecastDay.temp.min);
+        let localTimestamp =
+          forecastDay.dt + response.data.timezone_offset * 1000;
+        forecastHTML += `
+      <div class= "col-2">
+      <div class= "tempweekly-date">${formatDate(forecastDay.dt)}</div>
+          <img src="http://openweathermap.org/img/wn/${
+            forecastDay.weather[0].icon
+          }@2x.png" alt="${forecastDay.weather[0].description}" width="45" />
+      <div class="tempweekly-temp">
+       <span class="tempweekly-temp-max">${Math.round(
+         forecastDay.temp.max
+       )}°</span>
+        <span class="tempweekly-temp-min">${Math.round(
+          forecastDay.temp.min
+        )}°</span>
+      </div>
+    </div>`;
+      } else {
+        // Handle the case where timestamp or timezoneOffset is not available
+        console.error(
+          "Invalid timestamp or timezoneOffset for forecast day:",
+          forecastDay
+        );
+      }
+    }
+  });
+
+  forecastHTML += `</div>`;
+  forecastElt.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  let apiKey = "eb9542c65e739e0fb25ade97c749e2aa";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&exclude=current,minutely,hourly&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(forecastTemp);
+}
+
 // Function to fetch weather data by geolocation
 function searchPos(position) {
   let apiKey = "eb9542c65e739e0fb25ade97c749e2aa";
@@ -105,6 +173,7 @@ function searchPos(position) {
       console.error("Error fetching weather data:", error);
     });
 }
+
 document.addEventListener("DOMContentLoaded", function () {
   let form = document.querySelector(".input-city");
   let cityInput = document.querySelector("#cityInput");
@@ -133,4 +202,62 @@ document.addEventListener("DOMContentLoaded", function () {
       searchCity(city);
     }
   });
+
+  function searchCity(city) {
+    let apiKey = "eb9542c65e739e0fb25ade97c749e2aa";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+    axios
+      .get(apiUrl)
+      .then(function (response) {
+        if (response.data && response.data.timezone) {
+          let timezoneOffset = response.data.timezone;
+          let coordinates = {
+            lat: response.data.coord.lat,
+            lon: response.data.coord.lon,
+          };
+          showTemperature(response);
+          getForecast(coordinates);
+
+          updateDateTime(timezoneOffset);
+          setInterval(() => updateDateTime(timezoneOffset), 60000);
+        } else {
+          console.error("Invalid timezone data:", response.data);
+        }
+      })
+      .catch((error) => {
+        console.error("Error fetching weather data:", error);
+      });
+  } // Update the date and time
+  function updateDateTime(timezoneOffset) {
+    const dateTimeText = document.querySelector("#datetimeText");
+    const currentTimestamp = Math.floor(Date.now() / 1000);
+
+    const localTimestamp = currentTimestamp + timezoneOffset;
+
+    const date = new Date(localTimestamp * 1000);
+    const days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+    const day = days[date.getUTCDay()];
+    let hours = date.getUTCHours();
+    if (hours < 10) {
+      hours = `0${hours}`;
+    }
+    let minutes = date.getUTCMinutes();
+    if (minutes < 10) {
+      minutes = `0${minutes}`;
+    }
+
+    dateTimeText.innerHTML = `${day}, ${hours}:${minutes}`;
+  }
+
+  updateDateTime(timezoneOffset);
+  setInterval(() => updateDateTime(timezoneOffset), 60000);
 });
